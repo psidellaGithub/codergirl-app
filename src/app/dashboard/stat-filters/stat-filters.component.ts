97ca0944e968f617fffda4b986272c5bc588010d
priyanka <psidellagithub.com>
2018-09-26 20:13:38 -0500

    routing

---
+++ src/app/dashboard/stat-filters/stat-filters.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cg-stat-filters',
+  templateUrl: './stat-filters.component.html',
+  styleUrls: ['./stat-filters.component.css']
+})
+export class StatFiltersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/dashboard/video-dashboard/video-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cg-video-dashboard',
+  templateUrl: './video-dashboard.component.html',
+  styleUrls: ['./video-dashboard.component.css']
+})
+export class VideoDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
