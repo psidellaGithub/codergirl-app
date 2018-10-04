846158afb12d6c9b26c07e1f9f960dad6169aa00
priyanka <psidellagithub.com>
2018-10-03 20:02:45 -0500

    routing fixed

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -4,10 +4,16 @@ import { VideoDashboardComponent } from './video-dashboard/video-dashboard.compo
 import { VideoListComponent } from './video-list/video-list.component';
 import { VideoPlayerComponent } from './video-player/video-player.component';
 import { StatFiltersComponent } from './stat-filters/stat-filters.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', component: VideoDashboardComponent }
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
   ],
   declarations: [VideoDashboardComponent, VideoListComponent, VideoPlayerComponent, StatFiltersComponent]
 })
--- src/app/dashboard/video-player/video-player.component.ts
+++ src/app/dashboard/video-player/video-player.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+// import { Httpclient } from '@angular/common';
 @Component({
   selector: 'cg-video-player',
   templateUrl: './video-player.component.html',
   styleUrls: ['./video-player.component.css']
 })
 export class VideoPlayerComponent implements OnInit {
-
+  selcetedVideoId: string | undefined;
+  videos = [];
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+/* pickVideo(video: any); {
+  this.selctedVideoId = video.id;
+} */
+
